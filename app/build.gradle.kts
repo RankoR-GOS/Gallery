fdd9d1fae6a25fcc56ab6658b8e593b412b72c3b
Artem Smirnov <artem@smirnov.page>
2026-05-04 22:35:05 +0300

    Use our own version code and name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = baseApplicationId
         minSdk = 29
         targetSdk = 37
-        versionCode = 42101
-        versionName = "4.2.1"
+        versionCode = 1
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
