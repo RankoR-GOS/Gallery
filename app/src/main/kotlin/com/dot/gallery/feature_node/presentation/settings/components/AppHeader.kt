b9dc851ad492967f9db364db44dfba4094002996
Artem Smirnov <artem@smirnov.page>
2026-05-21 22:42:18 +0300

    Clarify that donations are going to ReFra

--- app/src/main/kotlin/com/dot/gallery/feature_node/presentation/settings/components/AppHeader.kt
+++ app/src/main/kotlin/com/dot/gallery/feature_node/presentation/settings/components/AppHeader.kt
@@ -43,6 +43,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -179,7 +180,10 @@ fun SettingsAppHeader() {
                 ) {
                     Icon(painter = donateImage, contentDescription = null)
                     Spacer(modifier = Modifier.width(8.dp))
-                    Text(text = donateTitle)
+                    Text(
+                        text = donateTitle,
+                        textAlign = TextAlign.Center,
+                    )
                 }
                 Button(
                     onClick = { uriHandler.openUri(githubUrl) },
