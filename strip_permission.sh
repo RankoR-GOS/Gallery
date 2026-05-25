fabeb3c18cecaeb7266b19804858af7015febe8e
Artem Smirnov <artem@smirnov.page>
2026-05-26 01:07:33 +0300

    Remove scripts not needed anymore

--- strip_permission.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sed \
-  -e '/<uses-permission android:name="android.permission.INTERNET"\/>/d' \
-  -e '/<uses-permission android:name="android.permission.ACCESS_NETWORK_STATE"\/>/d' \
-  app/src/main/AndroidManifest.xml > app/src/main/AndroidManifest2.xml
-mv app/src/main/AndroidManifest2.xml app/src/main/AndroidManifest.xml
