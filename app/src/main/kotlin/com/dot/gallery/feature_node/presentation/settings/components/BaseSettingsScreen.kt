waxew/AS-Negar

fix: preserve scroll state in settings categories when navigating to detail pages

app/src/main/kotlin/com/dot/gallery/feature_node/presentation/settings/components/BaseSettingsScreen.kt
@@ -5,7 +5,9 @@ import androidx.compose.foundation.layout.calculateEndPadding
 import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.LargeTopAppBar
 import androidx.compose.material3.MaterialTheme
@@ -31,7 +33,8 @@ fun BaseSettingsScreen(
         SettingsItem(it)
     },
     topContent: @Composable (() -> Unit)? = null,
-    bottomContent: @Composable (() -> Unit)? = null
+    bottomContent: @Composable (() -> Unit)? = null,
+    listState: LazyListState = rememberLazyListState()
 ) {
     val scrollBehavior =
         TopAppBarDefaults.exitUntilCollapsedScrollBehavior(rememberTopAppBarState())
@@ -47,6 +50,7 @@ fun BaseSettingsScreen(
         }
     ) { padding ->
         LazyColumn(
+            state = listState,
             modifier = Modifier
                 .fillMaxSize(),
             contentPadding = PaddingValues(

RankoR-GOS/Gallery

Keep category and palette scroll state above list/detail composition branches. Omit removed security settings and retain the current settings UI.
Upstream-commit: ebc5b5a4483baea42e2f7237af5802dc519c7fba

app/src/main/kotlin/com/dot/gallery/feature_node/presentation/settings/components/BaseSettingsScreen.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.calculateEndPadding
 import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -30,7 +32,8 @@ fun BaseSettingsScreen(
     settingsBuilder: @Composable (SettingsEntity, Int) -> Unit = { it, index ->
         SettingsItem(it)
     },
-    topContent: @Composable (() -> Unit)? = null
+    topContent: @Composable (() -> Unit)? = null,
+    listState: LazyListState = rememberLazyListState(),
 ) {
     val scrollBehavior =
         TopAppBarDefaults.exitUntilCollapsedScrollBehavior(rememberTopAppBarState())
@@ -46,6 +49,7 @@ fun BaseSettingsScreen(
         }
     ) { padding ->
         LazyColumn(
+            state = listState,
             modifier = Modifier
                 .fillMaxSize(),
             contentPadding = PaddingValues(